138ea9a7dadb76fc269053c06cfddc38aecd5d74
Stepan <107657774+SMamashin@users.noreply.github.com>
2023-06-21 18:17:33 +0300

    Add files via upload

--- pg.py
+++ pg.py
@@ -1,23 +1,32 @@
 import random
-from ui.pasgen_ui import Ui_Form
+import configparser
+import datetime
+from ui.passgen_ui import Ui_Form
 from PyQt5 import uic, QtGui
 from PyQt5.QtWidgets import QApplication
 
-Form, Window = uic.loadUiType('ui/passgen.ui')
+Form, Window = uic.loadUiType('C:/dev/projects/passgenerator/ui/passgen.ui')
 
 app = QApplication([])
 window = Window()
 form = Form()
 form.setupUi(window)
 window.show()
+cfg = configparser.ConfigParser()
 
 # Generatepassword by S-Mamashin
 
 def generator(length):
     allow = "1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ"
-    password = "".join(random.choice(allow) for c in range(length) ) 
+    password = "".join(random.choice(allow) for c in range(length) )
+    time = datetime.datetime.now(tz=None)
 
-    return form.lineEdit.setText(password), form.label.setText(f"Сгенерирован пароль из {length} символов!") 
+    reg_password = open('C:/dev/projects/PassGenerator/source/passwords.txt', 'a+')
+    reg_password.write(f'\n\nПароль: {password} , был сгенерирован: {time}') 
+
+    line_edit = form.lineEdit.setText(password), form.label.setText(f"Сгенерирован пароль из {length} символов!"), form.label_3.setText(f"Последний пароль: {password}")
+
+    return line_edit
 
 form.pushButton.clicked.connect(lambda: generator(16))
 form.pushButton_2.clicked.connect(lambda: generator(8))
